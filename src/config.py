d061d9b6327708ebff07b2ebec0c03e1ec131fd0
ZRunner <z.runner.mc@gmail.com>
2022-09-21 11:37:17 -0400

    fix config

--- src/config.py
+++ src/config.py
@@ -8,48 +8,51 @@ FPS: int = 60
 # Friction coefficient used in acceleration calculs
 FRICTION: float = 0.2
 
+# Approximative Initial food quantities per generator
+INITIAL_FOOD_QUANTITY: int = 200
+
 # Creatures count at the beginning
 CREATURES_COUNT: int = 200
 
 # Energy points each creature has at the beginning
-CREATURE_STARTING_ENERGY = 30
+CREATURE_STARTING_ENERGY: int = 30
 
 # Minimum food ready to be digested at the beginning
-CREATURE_MIN_STARTING_DIGESTING_POINTS = 6
+CREATURE_MIN_STARTING_DIGESTING_POINTS: int = 6
 
 # Maximum creature acceleration (on each axis)
-MAX_CREATURE_ACC = 0.0001
+MAX_CREATURE_ACC: float = 0.0001
 
 # Maximum creature veolocity (on each axis)
-MAX_CREATURE_VEL = 0.1
+MAX_CREATURE_VEL: float = 0.08
 
 # Maximum reature velocity decrease coefficient (on each axis)
-CREATURE_DECELERATION = 1e-5
+CREATURE_DECELERATION: float = 1e-5
 
 # Amount of energy lost by idle creatures each second
-CREATURE_STILL_ENERGY = 0.15
+CREATURE_STILL_ENERGY: float = 0.15
 
 # Minimum creature size
-MIN_CREATURE_SIZE = 1
+MIN_CREATURE_SIZE: int = 1
 
 # Average and simga creature size (using a gaussian curve)
-CREATURE_SIZE_AVG = 4
-CREATURE_SIZE_SIGMA = 3
+CREATURE_SIZE_AVG: int = 4
+CREATURE_SIZE_SIGMA: int = 3
 
 # Minimum connections number in a creature neural network
-CREATURES_MIN_CONNECTIONS = 2
+CREATURES_MIN_CONNECTIONS: int = 2
 
 # Maximum connections number in a creature neural network
-CREATURES_MAX_CONNECTIONS = 10
+CREATURES_MAX_CONNECTIONS: int = 12
 
 # Maximum transitions ("hidden") neurons in a creature neural network
-CREATURES_MAX_HIDDEN_NEURONS = 20
+CREATURES_MAX_HIDDEN_NEURONS: int = 25
 
 # Maximum visibility of emitted light
-CREATURE_MAX_LIGHT_DISTANCE_EMISSION = 200
+CREATURE_MAX_LIGHT_DISTANCE_EMISSION: int = 200
 
 # Minimum neuron value for a creature to reproduce with a partner
-CREATURE_MIN_REPRODUCTION_STATE = 0.2
+CREATURE_MIN_REPRODUCTION_STATE: float = 0.1
 
 # Minimum time in seconds between two reproductions from the same creature
-CREATURE_REPRO_COOLDOWN = 15
+CREATURE_REPRO_COOLDOWN: int = 20
